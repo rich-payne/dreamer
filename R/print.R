01150c7f1decd22b8598bafd475a46a9b4ba193a
Richard Payne <richard.payne@lilly.com>
2022-07-13 15:23:23 -0400

    add print methods to models

--- R/print.R
+++ R/print.R
@@ -35,3 +35,46 @@ print.dreamer_mcmc <- function(x, ...) {
   cat(post, sep = "\n")
   return(invisible())
 }
+
+#' @export
+print.dreamer_continuous <- function(x, ...) {
+  print_model(x, "continuous")
+}
+
+#' @export
+print.dreamer_binary <- function(x, ...) {
+  print_model(x, "binary")
+}
+
+print_model <- function(x, response_type) {
+  hyparms <- x
+  hyparms$longitudinal <- NULL
+  hyparms <- get_hyparms(hyparms) %>%
+    knitr::kable(format = "pipe")
+  long <- get_hyparms(x$longitudinal)
+  if (!is.null(long)) {
+    long <- knitr::kable(long, format = "pipe")
+  }
+  cat("-------------\n")
+  cat("dreamer Model\n")
+  cat("-------------\n")
+  cat(paste0("type    : ", attr(x, "type"), "\n"))
+  cat(paste0("response: ", response_type, "\n"))
+  cat("\n")
+  cat("Dose Response\n")
+  cat(hyparms, sep = "\n")
+  
+  if (!is.null(long)) {
+    cat("\n")
+    cat("Longitudinal\n")
+    cat(long, sep = "\n")
+  }
+}
+
+get_hyparms <- function(hyparms) {
+  if (is.null(hyparms)) return()
+  hyparms <- data.frame(
+    hyperparameter = names(hyparms),
+    value = unlist(hyparms, use.names = FALSE)
+  )
+}
